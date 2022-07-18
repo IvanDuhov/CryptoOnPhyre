e88ada456e082c9aa52f10fac794e3d294e4aab0
Ivan Duhov <iduhov@carbonblack.com>
2022-07-18 19:36:59 +0300

    Adding integration with Coin market cap api

--- src/main/java/com/crypto/cryptopricechecker/service/PriceCheckerService.java
+++ src/main/java/com/crypto/cryptopricechecker/service/PriceCheckerService.java
@@ -2,6 +2,7 @@ package com.crypto.cryptopricechecker.service;
 
 import com.crypto.cryptopricechecker.utils.CryptoPriceProvider;
 import com.crypto.cryptopricechecker.utils.StringValidator;
+import java.net.HttpRetryException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,7 @@ public class PriceCheckerService {
 
     private final CryptoPriceProvider cryptoPriceProvider;
 
-    public Double getPrice(String ticker) {
-        ticker = ticker.toUpperCase();
-
+    public Double getPrice(String ticker) throws HttpRetryException {
         if (!StringValidator.isValidTicker(ticker)) {
             log.error("Sorry, the ticker: {} is in invalid format!", ticker);
             throw new IllegalArgumentException("Sorry, the ticker format is not valid!");
