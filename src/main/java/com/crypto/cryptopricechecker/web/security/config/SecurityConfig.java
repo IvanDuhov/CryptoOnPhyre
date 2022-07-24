3bcc572474f7c1ae6ae8e020d717c6acf05a7caf
Ivan Duhov <iduhov@carbonblack.com>
2022-07-25 00:08:56 +0300

    Make the /account/* endpoints accessible

--- src/main/java/com/crypto/cryptopricechecker/web/security/config/SecurityConfig.java
+++ src/main/java/com/crypto/cryptopricechecker/web/security/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
@@ -23,6 +24,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private UserService userService;
 
     @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().requestMatchers(new AntPathRequestMatcher("/v1/account/**"));
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()  //No Http Basic Login
@@ -31,12 +37,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout().disable()  //No Logout
                 //No Session
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().addFilterBefore(new ContextHeaderPreAuthFilter(authenticationManager(),
-                        userService),
-                RequestHeaderAuthenticationFilter.class)
+                .and()
+                .addFilterBefore(
+                        new ContextHeaderPreAuthFilter(authenticationManager(), userService),
+                        RequestHeaderAuthenticationFilter.class)
                 .authorizeRequests()
-                    .requestMatchers(new AntPathRequestMatcher("/**")).authenticated()
-                    .anyRequest().denyAll();
+                .requestMatchers(new AntPathRequestMatcher("/**")).authenticated();
     }
 
     @Override
@@ -47,11 +53,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationProvider authTokenHeaderAuthProvider() {
         return new AuthHeaderProvider();
-    }
-
-    @Bean
-    public ContextHeaderPreAuthFilter authHeaderProvider() throws Exception {
-        return new ContextHeaderPreAuthFilter(authenticationManager(), userService);
     }
 
 }
