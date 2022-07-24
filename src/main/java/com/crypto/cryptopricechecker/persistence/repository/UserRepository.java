961dfe7fa4b194820b62567637e03894824964dd
Ivan Duhov <duhovgames@gmail.com>
2022-07-24 19:44:00 +0300

    Adding Spring security (#9)

--- src/main/java/com/crypto/cryptopricechecker/persistence/repository/UserRepository.java
+++ src/main/java/com/crypto/cryptopricechecker/persistence/repository/UserRepository.java
@@ -2,8 +2,14 @@ package com.crypto.cryptopricechecker.persistence.repository;
 
 import com.crypto.cryptopricechecker.persistence.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import io.lettuce.core.dynamic.annotation.Param;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+    User findByAuthToken(String authToken);
+
 }
